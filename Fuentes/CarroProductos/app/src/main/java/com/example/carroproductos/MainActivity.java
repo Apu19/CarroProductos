c9d70d2aa739b0867a6ecc58a343dcd76716c761
Emmanuel <calertios12@gmail.com>
2020-10-27 02:06:11 -0600

    Finalización de la app
    Darle toda la navegación
    Agregar detalles a la interfaz
    Arreglar errores

--- Fuentes/CarroProductos/app/src/main/java/com/example/carroproductos/MainActivity.java
+++ Fuentes/CarroProductos/app/src/main/java/com/example/carroproductos/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.carroproductos;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.carroproductos.adapters.ProductosListAdapter;
@@ -28,5 +31,15 @@ public class MainActivity extends AppCompatActivity {
         this.productosLv = findViewById(R.id.productos_lv);
         this.adapter = new ProductosListAdapter(this,R.layout.productos_list,this.productos);
         this.productosLv.setAdapter(this.adapter);
+        //Agregar un listener a los elementos de la lista
+        this.productosLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Producto producto = productos.get(i);
+                Intent intent = new Intent(MainActivity.this,ProductoViewActivity.class);
+                intent.putExtra("producto",producto);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ Fuentes/CarroProductos/app/src/main/java/com/example/carroproductos/ProductoViewActivity.java
@@ -0,0 +1,50 @@
+package com.example.carroproductos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.carroproductos.dto.Producto;
+import com.squareup.picasso.Picasso;
+
+public class ProductoViewActivity extends AppCompatActivity {
+    TextView nombreProdTv;
+    TextView descProdTv;
+    ImageView imgProd;
+    TextView precioProdTv;
+    Producto producto;
+    Toolbar toolbar;
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_producto_view);
+        //referencia del toolbar
+        this.toolbar = findViewById(R.id.toolbar);
+        this.setSupportActionBar(this.toolbar);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        this.getSupportActionBar().setDisplayShowHomeEnabled(true);
+        this.nombreProdTv = findViewById(R.id.nombre_prod_view_txt);
+        this.imgProd = findViewById(R.id.img_prod_view);
+        this.descProdTv = findViewById(R.id.desc_prod_view);
+        this.precioProdTv = findViewById(R.id.precio_prod_view);
+        if (getIntent().getExtras() != null) {
+            //El pasaje de avión venia con yapa
+            //Me enviaron algo en el intent
+            this.producto = (Producto) getIntent().getSerializableExtra("producto");
+            this.nombreProdTv.setText(producto.getNombre());
+            Picasso.get().load(this.producto.getFoto()).resize(500,500).centerCrop().into(this.imgProd);
+            this.descProdTv.setText(this.producto.getDescripcion());
+            this.precioProdTv.setText("$" + this.producto.getPrecio());
+        }
+    }
+}
